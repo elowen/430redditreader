24d1133a188f9f974585799b388e2bd8b72d6459
elowen <eowen@mail.umw.edu>
2014-03-13 14:46:53 -0400

    added wordpress plugin standard header

--- umwCustomReader.php
+++ umwCustomReader.php
@@ -1,1 +1,12 @@
+<?php
+/**
+ * Plugin Name: umwCustomReader
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates-doesn't exist yet
+ * Description: Customizes the Domain of One's Own project to implement comment and content voting
+ * Version: 1.0
+ * Author: Dunham, Khalifa, Owen
+ * Author URI: http://URI_Of_The_Plugin_Author-doesn't exist yet
+ * License: A "Slug" license name e.g. GPL2----WHAT IS THIS
+ */
+
 /* our new php file for the reddit reader. insert amazing content. */
